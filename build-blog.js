e874f8b797e62fc02df2657ebe5543f3f6a8b6dc
Todd Bracher <toddbracher@macbookpro.mynetworksettings.com>
2026-01-10 21:52:58 -0500

    Fix blog date timezone issue - parse as local date

--- build-blog.js
+++ build-blog.js
@@ -115,7 +115,9 @@ function getProperty(page, name) {
 
 function formatDate(dateStr) {
   if (!dateStr) return '';
-  const date = new Date(dateStr);
+  // Parse as local date to avoid timezone shift
+  const [year, month, day] = dateStr.split('-').map(Number);
+  const date = new Date(year, month - 1, day);
   return date.toLocaleDateString('en-US', {
     year: 'numeric',
     month: 'long',
